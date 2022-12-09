4649768153f95cf94d2b083a789558286e5cfa5b
Santiago Gonzalez <parrasanti.gonzalez@gmail.com>
2022-12-09 17:50:04 -0500

    Agregue un efecto hover a la barra de navegacion, y termine la seccion de 'sobre mi' agregando mi imagen personal y cierta informacion

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 const lista = document.querySelector('.lista-navegador');
 const abrirMenu = document.querySelector('.abrir-menu');
 const cerrarMenu = document.querySelector('.cerrar-menu');
+const main = document.querySelector('main')
 
 function alternarBoton() {
+    main.classList.toggle("inactive");
     lista.classList.toggle("menu-abierto");
 }
 
